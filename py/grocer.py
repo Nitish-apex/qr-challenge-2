b91ccc359bb47b05e5316d6581fa1e852118bdae
Siddhant <raosiddhant99@gmail.com>
2019-10-06 16:59:34 +0530

    added simple py prog and readme

---
+++ py/grocer.py
@@ -0,0 +1,17 @@
+#prices of items
+prices = {'apple': 10, 'tomato':5 'banana': 1, 'potato':5}
+
+#items purchased
+my_purchase = {
+    'apple': 1,
+    'banana': 6,
+    'potato': 5
+}
+
+grocery_bill = 0
+
+for purchase in my_purchase:
+	grocery_bill += my_purchase[purchase] * prices[purchase]
+
+#total bill
+print ('I owe the grocer %.2f rupees' % grocery_bill)
